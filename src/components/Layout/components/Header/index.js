d4f20c7ab149690f260345b1bad1a470d45f99ee
nam18022001 <hnvnam.19it3@vku.udn.vn>
2022-09-30 18:25:39 +0700

    Header UI #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,31 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './Header.module.scss';
+import images from '~/assets/image/index';
+import { faCircleXmark, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('content')}></div>
+      <div className={cx('content')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Logo" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search Accouts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
+      </div>
     </header>
   );
 }
